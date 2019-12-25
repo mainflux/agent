a4c5e6bd00248a842dc2e85803d76de5a6bce723
Mirko Teodorovic <mirko.teodorovic@gmail.com>
2019-12-25 13:34:32 +0100

    add logic for monitoring service status
    Signed-off-by: Mirko Teodorovic <mirko.teodorovic@gmail.com>

--- internal/app/agent/register/register.go
+++ internal/app/agent/register/register.go
@@ -1,9 +1,7 @@
 package register
 
 import (
-	"fmt"
 	"strings"
-	"time"
 
 	nats "github.com/nats-io/go-nats"
 )
@@ -15,49 +13,38 @@ const (
 var _ Service = (*register)(nil)
 
 type Service interface {
-	Add(a Application) error
-	Remove(a Application) error
-	List() ([]Application, error)
+	Applications() (map[string]*Application, error)
 }
 
 type register struct {
 	nc   *nats.Conn
-	apps map[string]map[string]Application
+	apps map[string]*Application
 }
 
 func New(nc *nats.Conn) (Service, error) {
 	r := register{
 		nc:   nc,
-		apps: make(map[string]map[string]Application),
+		apps: make(map[string]*Application),
 	}
 
 	_, err := r.nc.Subscribe(HEARTBEAT, func(msg *nats.Msg) {
 		sub := msg.Subject
 		appname := strings.Split(sub, ".")[1]
-		if amap, ok := r.apps[appname]; !ok {
-			r.apps[appname] = make(map[string]Application)
-			num := 0
-			regname := fmt.Sprintf("%s-%d", appname, num)
-			a := Application{
-				Name:     appname,
-				RegName:  regname,
-				Num:      num,
-				LastSeen: time.Now(),
-				Status:   ONLINE,
-			}
-			r.apps[appname][regname] = a
+		// Service name is extracted from the subtopic
+		// if there is multiple instances of the same service
+		// we will have to add another distinction
+		if _, ok := r.apps[appname]; !ok {
+			a := NewApplication(appname)
+			r.apps[appname] = a
 			return
 		}
-		amap[appname]
-
+		a := r.apps[appname]
+		a.Update()
 	})
 	if err != nil {
-		return r, err
+		return &r, err
 	}
-
-	return r, nil
+	return &r, nil
 }
 
-func (r register) Add(a Application) error      { return nil }
-func (r register) Remove(a Application) error   { return nil }
-func (r register) List() ([]Application, error) { return []Application{}, nil }
+func (r *register) Applications() (map[string]*Application, error) { return r.apps, nil }
