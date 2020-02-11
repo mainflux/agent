652a6283342e36743b18c0b357c9e7db4446e4fd
Mirko Teodorovic <mirko.teodorovic@gmail.com>
2020-02-11 12:28:26 +0100

    fix vendor
    Signed-off-by: Mirko Teodorovic <mirko.teodorovic@gmail.com>

--- internal/pkg/bootstrap/bootstrap.go
+++ internal/pkg/bootstrap/bootstrap.go
@@ -47,8 +47,6 @@ type infraConfig struct {
 	ExportConfig export.Config `json:"export_config"`
 }
 
-const exportConfigFile = "export.toml"
-
 // Bootstrap - Retrieve device config
 func Bootstrap(cfg Config, logger log.Logger, file string) error {
 	retries, err := strconv.ParseUint(cfg.Retries, 10, 64)
