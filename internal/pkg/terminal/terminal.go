49e13c4eb5d9d6d686e2cbf2e1fb46028ae75f62
Mirko Teodorovic <mirko.teodorovic@gmail.com>
2020-03-20 14:23:00 +0100

    add terminal session
    Signed-off-by: Mirko Teodorovic <mirko.teodorovic@gmail.com>

---
+++ internal/pkg/terminal/terminal.go
@@ -0,0 +1,85 @@
+package terminal
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+	"strings"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+)
+
+type term struct {
+	uuid string
+	mqtt *mqtt.Client
+}
+
+type Session interface {
+	io.Writer
+}
+
+func NewSession(uuid string, mqtt *mqtt.Client) Session {
+	return &term{
+		uuid: uuid,
+		mqtt: mqtt,
+	}
+}
+
+func (t *term) Write(p []byte) (n int, err error) {
+	os.Stdin
+
+}
+
+func maine() {
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Print("> ")
+		// Read the keyboad input.
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+
+		// Handle the execution of the input.
+		if err = execInput(input); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+	}
+}
+
+// ErrNoPath is returned when 'cd' was called without a second argument.
+var ErrNoPath = errors.New("path required")
+
+func execInput(input string) error {
+	// Remove the newline character.
+	input = strings.TrimSuffix(input, "\n")
+
+	// Split the input separate the command and the arguments.
+	args := strings.Split(input, " ")
+
+	// Check for built-in commands.
+	switch args[0] {
+	case "cd":
+		// 'cd' to home with empty path not yet supported.
+		if len(args) < 2 {
+			return ErrNoPath
+		}
+		// Change the directory and return the error.
+		return os.Chdir(args[1])
+	case "exit":
+		os.Exit(0)
+	}
+
+	// Prepare the command to execute.
+	cmd := exec.Command(args[0], args[1:]...)
+
+	// Set the correct output device.
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+
+	// Execute the command and return the error.
+	return cmd.Run()
+}
