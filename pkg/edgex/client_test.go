2dc7996ae07a68f282ce383f93e412889b17e3d6
SammyOina <sammyoina@gmail.com>
2023-09-28 12:23:18 +0300

    Add test for PushOperation function in edgex package
    This commit adds a new test for the PushOperation function in the edgex package. The test creates a mock HTTP server and checks the HTTP request method, URL, and request body. The test ensures that the function correctly handles the POST request with the expected URL and request body.
    
    Signed-off-by: SammyOina <sammyoina@gmail.com>

---
+++ pkg/edgex/client_test.go
@@ -0,0 +1,158 @@
+package edgex
+
+import (
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/mainflux/mainflux/logger"
+)
+
+func TestPushOperation(t *testing.T) {
+	// Create a mock HTTP server
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Check the HTTP request
+		if r.Method != http.MethodPost {
+			t.Errorf("Expected POST request, got %s", r.Method)
+		}
+
+		// Check the URL
+		expectedURL := "/operation"
+		if r.URL.String() != expectedURL {
+			t.Errorf("Expected URL %s, got %s", expectedURL, r.URL.String())
+		}
+
+		// Check the request body
+		expectedBody := `{"action":"start","services":["service1","service2"]}`
+		bodyBytes, _ := io.ReadAll(r.Body)
+		if string(bodyBytes) != expectedBody {
+			t.Errorf("Expected request body %s, got %s", expectedBody, string(bodyBytes))
+		}
+
+		// Respond with a dummy response
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Response"))
+	}))
+	defer server.Close()
+
+	// Create an edgexClient with the mock server URL
+	client := NewClient(server.URL+"/", logger.NewMock())
+
+	// Test PushOperation
+	response, err := client.PushOperation([]string{"start", "service1", "service2"})
+	if err != nil {
+		t.Errorf("Error calling PushOperation: %v", err)
+	}
+
+	expectedResponse := "Response"
+	if response != expectedResponse {
+		t.Errorf("Expected response %s, got %s", expectedResponse, response)
+	}
+}
+
+func TestFetchConfig(t *testing.T) {
+	// Create a mock HTTP server
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Check the HTTP request
+		if r.Method != http.MethodGet {
+			t.Errorf("Expected GET request, got %s", r.Method)
+		}
+
+		// Check the URL
+		expectedURL := "/config/start,service1,service2"
+		if r.URL.String() != expectedURL {
+			t.Errorf("Expected URL %s, got %s", expectedURL, r.URL.String())
+		}
+
+		// Respond with a dummy response
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Response"))
+	}))
+	defer server.Close()
+
+	// Create an edgexClient with the mock server URL
+	client := NewClient(server.URL+"/", logger.NewMock())
+
+	// Test FetchConfig
+	response, err := client.FetchConfig([]string{"start", "service1", "service2"})
+	if err != nil {
+		t.Errorf("Error calling FetchConfig: %v", err)
+	}
+
+	expectedResponse := "Response"
+	if response != expectedResponse {
+		t.Errorf("Expected response %s, got %s", expectedResponse, response)
+	}
+}
+
+func TestFetchMetrics(t *testing.T) {
+	expectedResponse := "Response"
+
+	// Create a mock HTTP server
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Check the HTTP request
+		if r.Method != http.MethodGet {
+			t.Errorf("Expected GET request, got %s", r.Method)
+		}
+
+		// Check the URL
+		expectedURL := "/metrics/start,service1,service2"
+		if r.URL.String() != expectedURL {
+			t.Errorf("Expected URL %s, got %s", expectedURL, r.URL.String())
+		}
+
+		// Respond with a dummy response
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(expectedResponse))
+	}))
+	defer server.Close()
+
+	// Create an edgexClient with the mock server URL
+	client := NewClient(server.URL+"/", logger.NewMock())
+
+	// Test FetchMetrics
+	response, err := client.FetchMetrics([]string{"start", "service1", "service2"})
+	if err != nil {
+		t.Errorf("Error calling FetchMetrics: %v", err)
+	}
+
+	if response != expectedResponse {
+		t.Errorf("Expected response %s, got %s", expectedResponse, response)
+	}
+}
+
+func TestPing(t *testing.T) {
+	// Create a mock HTTP server
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Check the HTTP request
+		if r.Method != http.MethodGet {
+			t.Errorf("Expected GET request, got %s", r.Method)
+		}
+
+		// Check the URL
+		expectedURL := "/ping"
+		if r.URL.String() != expectedURL {
+			t.Errorf("Expected URL %s, got %s", expectedURL, r.URL.String())
+		}
+
+		// Respond with a dummy response
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Response"))
+	}))
+	defer server.Close()
+
+	// Create an edgexClient with the mock server URL
+	client := NewClient(server.URL+"/", logger.NewMock())
+
+	// Test Ping
+	response, err := client.Ping()
+	if err != nil {
+		t.Errorf("Error calling Ping: %v", err)
+	}
+
+	expectedResponse := "Response"
+	if response != expectedResponse {
+		t.Errorf("Expected response %s, got %s", expectedResponse, response)
+	}
+}
