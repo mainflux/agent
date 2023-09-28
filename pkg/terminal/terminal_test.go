37ccb0497d255a6c4868cb343d9b5e38cef4ce91
SammyOina <sammyoina@gmail.com>
2023-09-28 11:43:45 +0300

    Add terminal tests
    Refactor build workflow
    
    The build22.yml file has been deleted. This commit removes the build22.yml file, which was used to create and publish a Docker image. The file is no longer needed as it is no longer being used in the project.
    
    Signed-off-by: SammyOina <sammyoina@gmail.com>

---
+++ pkg/terminal/terminal_test.go
@@ -0,0 +1,73 @@
+package terminal
+
+import (
+	"testing"
+	"time"
+
+	"github.com/mainflux/mainflux/logger"
+	"github.com/stretchr/testify/assert"
+)
+
+// MockPublish is a mock function for the publish function used in NewSession.
+func MockPublish(channel, payload string) error {
+	return nil
+}
+
+func TestSessionWrite(t *testing.T) {
+	publish := MockPublish
+
+	uuid := "test-uuid"
+	timeout := 5 * time.Second
+
+	session, err := NewSession(uuid, timeout, publish, logger.NewMock())
+	if err != nil {
+		t.Fatalf("Expected no error, but got: %v", err)
+	}
+
+	// Simulate writing data to the session
+	data := []byte("test data")
+	n, err := session.Write(data)
+	if err != nil {
+		t.Fatalf("Expected no error, but got: %v", err)
+	}
+	assert.Equal(t, len(data), n)
+}
+
+func TestSessionSend(t *testing.T) {
+	publish := MockPublish
+
+	uuid := "test-uuid"
+	timeout := 5 * time.Second
+
+	session, err := NewSession(uuid, timeout, publish, logger.NewMock())
+	if err != nil {
+		t.Fatalf("Expected no error, but got: %v", err)
+	}
+
+	// Simulate sending data to the session
+	data := []byte("test data")
+
+	if err = session.Send(data); err != nil {
+		t.Fatalf("Expected no error, but got: %v", err)
+	}
+}
+
+func TestSessionIsDone(t *testing.T) {
+	publish := MockPublish
+
+	uuid := "test-uuid"
+	timeout := 5 * time.Second
+
+	session, err := NewSession(uuid, timeout, publish, logger.NewMock())
+	if err != nil {
+		t.Fatalf("Expected no error, but got: %v", err)
+	}
+
+	// Wait for the "done" channel to be closed or for a timeout, and perform assertions accordingly.
+	select {
+	case <-session.IsDone():
+		// Session is done as expected.
+	case <-time.After(10 * time.Second):
+		t.Fatalf("Expected session to be done, but it is still running.")
+	}
+}
